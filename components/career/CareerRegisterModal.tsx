c880d9be0e013d5b50ced7b056a07958d91d37a3
박 종훈 <m05214@naver.com>
2020-12-16 17:24:17 +0900

    이력서 등록 모달 컴포넌트 수정
    이력서 등록 모달 컴포넌트에 이력서 제목, 경력서 요약, 이력년수, 연봉정보 필드 추가함

--- components/career/CareerRegisterModal.tsx
+++ components/career/CareerRegisterModal.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Modal from "@material-ui/core/Modal";
 import {createStyles, makeStyles, Theme} from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         careerRegisterModal: {
             position: 'absolute',
             width: 600,
-            height: 280,
+            height: 600,
             backgroundColor: theme.palette.background.paper,
             border: '2px solid #000',
             boxShadow: theme.shadows[1],
@@ -45,7 +47,38 @@ export default function CareerRegisterModal(props:CareerRegisterModalProps){
                         <main>
                             <div style={{margin: "15px" }}>
                                 <div style={{ backgroundColor: "#f5f6fa"}}>
+                                    <div className="w3-container" style={{margin: "8px"}}>
+                                        <table className="w3-table w3-bordered">
+                                            <thead></thead>
+                                            <tbody>
+                                            <tr>
+                                                <td style={{verticalAlign: "middle"}}>
+                                                    <TextField style={{marginLeft: "20px",  marginBottom: "5px"}} id="resumeName" name="resumeName" label="이력서 제목" error={false} />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style={{verticalAlign: "middle"}}>
+                                                    <textarea id="resumeSummary" name="resumeSummary" style={{width: "95%", height: "150px"}} />
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style={{verticalAlign: "middle"}}>
+                                                    <TextField style={{marginLeft: "20px",  marginBottom: "5px"}} id="resumeYears" name="resumeYears" label="경력년수" error={false} />
+                                                    <TextField style={{marginLeft: "20px",  marginBottom: "5px"}} id="resumeSalary" name="resumeSalary" label="연봉정" error={false} />
+                                                </td>
+                                            </tr>
+                                            </tbody>
+                                        </table>
+                                        <div style={{marginLeft: "80%", marginTop: "15px"}}>
+                                            <Button variant="contained" color="secondary" size="small" name="foreignTestSave" id="foreignTestSave" style={{marginRight: "5px"}}>
+                                                등록
+                                            </Button>
 
+                                            <Button variant="contained"  size="small" name="foreignTestCancle" id="foreignTestCancle">
+                                                취소
+                                            </Button>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </main>
